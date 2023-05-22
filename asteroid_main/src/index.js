34093d26bda816a7599ea3f87e40f5b2230ff70f
hannadarwish <hannadarwish@gmail.com>
2023-05-22 15:15:59 -0700

    started moving object

--- asteroid_main/src/index.js
+++ asteroid_main/src/index.js
@@ -3,3 +3,17 @@
 import MovingObject from "./moving_object";
 
 window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasEl = document.getElementById("game-canvas")
+    canvasEl.width = 500;
+    canvasEl.height = 700;
+    const ctx = canvasEl.getContext("2d")
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 500, 700);
+
+    ctx.fillStyle = "pink"
+    ctx.fillRect(50, 50, 80, 80)
+
+
+})
--- asteroid_main/src/moving_object.js
+++ asteroid_main/src/moving_object.js
@@ -1,3 +1,5 @@
+export default MovingObject;
+
 class MovingObject {
     constructor({ pos, vel, radius, color }) {
         this.pos = pos;
@@ -5,7 +7,25 @@ class MovingObject {
         this.radius = radius;
         this.color = color;
     }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
 }
+
+// MovingObject.prototype.draw = function(ctx) {
+//     return new MovingObject(
+        
+//     )
+// }
+
+
+
+
 
     // const mo = new MovingObject({
     //     pos: [30, 30],
@@ -14,4 +34,4 @@ class MovingObject {
     //     color: "#00FF00"
     // });
 
-export default MovingObject;
+
